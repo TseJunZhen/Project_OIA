a8ff85f0161de05dce6518f7600aea45c56a1cc2
TseJunZhen <tsejunzhen95@hotmail.com>
2017-04-25 21:03:24 +0800

    sick

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,17 +1,72 @@
 $(function() {
-  var b = $("#button");
-  var w = $("#wrapper");
-  var l = $("#list");
+  var b = $("#button_1");
+  var w = $("#wrapper_1");
+  var l = $("#list_1");
 
-  w.height(l.outerHeight(true));
-
+  w.height(0);
   b.click(function() {
-  
-    if(w.hasClass('open')) {
-      w.removeClass('open');
+
+    if(w.hasClass('open_1')) {
+      w.removeClass('open_1');
       w.height(0);
     } else {
-      w.addClass('open');
+      w.addClass('open_1');
+      w.height(l.outerHeight(true));
+    }
+
+  });
+});
+
+$(function() {
+  var b = $("#button_2");
+  var w = $("#wrapper_2");
+  var l = $("#list_2");
+
+  w.height(0);
+  b.click(function() {
+
+    if(w.hasClass('open_2')) {
+      w.removeClass('open_2');
+      w.height(0);
+    } else {
+      w.addClass('open_2');
+      w.height(l.outerHeight(true));
+    }
+
+  });
+});
+$(function() {
+  var b = $("#button_3");
+  var w = $("#wrapper_3");
+  var l = $("#list_3");
+
+  w.height(0);
+  b.click(function() {
+
+    if(w.hasClass('open_3')) {
+      w.removeClass('open_3');
+      w.height(0);
+    } else {
+      w.addClass('open_3');
+      w.height(l.outerHeight(true));
+    }
+
+  });
+});
+
+$(function() {
+  var b = $("#button_4");
+  var w = $("#wrapper_4");
+  var l = $("#list_4");
+
+  w.height(0);
+  b.click(function() {
+
+    if(w.hasClass('open_4')) {
+      w.removeClass('open_4');
+      w.height(0);
+    } else {
+      w.addClass('open_4');
       w.height(l.outerHeight(true));
     }
 
